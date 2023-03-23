4459784bbdb22eaaf65b4898335b0e13df896210
Rashidul Islam <rasidulislam71@gmail.com>
2023-03-23 22:37:25 +0530

    adding the react app again with proper file system

---
+++ medhavi_coaching_react/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'Right',
+            alignItems: 'Right',
+            height: '100vh'
+          }}
+        >
+          <h1>Medhavi Coaching is about giving to the students and the society, better Engineers and Doctors.</h1>
+        </div>
+      );
+    };
+
+export default About
