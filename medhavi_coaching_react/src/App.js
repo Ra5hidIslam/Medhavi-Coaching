535072f76c7a515a270c553b5e8c51bd54cbb381
Rashidul Islam <rasidulislam71@gmail.com>
2023-04-18 23:29:51 +0530

    added exam file

--- medhavi_coaching_react/src/App.js
+++ medhavi_coaching_react/src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home/Home"
 import About from './pages/About/About';
 import Courses from './pages/Courses/Courses';
-import Exams  from './pages/Exams/Exams'
+import ExamsPage from './pages/Exams/ExamsPage';
 import QuizList from './pages/Quiz/QuizList';
 // import Signin from './pages/Signin/Signin';
 import Prac from './pages/Prac/Prac';
@@ -22,7 +22,7 @@ return (
 		<Route path='/Home' exact  element={<Home/>}/>
 		<Route path='/About' element={<About/>} />
 		<Route path='/Courses' element={<Courses/>} />
-		<Route path='/Exams' element={<Exams/>} />
+		<Route path='/Exams' element={<ExamsPage/>} />
 		<Route path='/QuizList/*' element={<QuizList/>} />
 		{/* <Route exact path='/profile' render ={()=>{window.location.href ="/profile.html"}} /> */}
 		{/* <Route path='/Signin' element={<Signin/>} /> */}
