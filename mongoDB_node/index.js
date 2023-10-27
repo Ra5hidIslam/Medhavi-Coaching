a26dd071966e433522d369e7227b647f2c72b551
Rashid <rasidulislam71@gmail.com>
2023-10-28 01:31:46 +0530

    addign env

--- mongoDB_node/index.js
+++ mongoDB_node/index.js
@@ -44,7 +44,7 @@ app.use(morgan("common"));
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", env.REACT_APP_API_URL_ORIGIN); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
